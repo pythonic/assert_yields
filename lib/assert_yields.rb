cc2e3799bc352ceaaa82cc1103b66ca968f38647
Pythonic Pty Ltd <administrator@pythonic.com.au>
2008-11-19 12:34:39 +1100

    p.call now takes block for return value for methods like inject

--- lib/assert_yields.rb
+++ lib/assert_yields.rb
@@ -1,13 +1,23 @@
-# Copyright (c) 2007, 2008 Pythonic Pty. Ltd. http://www.pythonic.com.au/
+# Copyright (c) 2007, 2008 Pythonic Pty Ltd
+# http://www.pythonic.com.au/
 
-class Continuation
-  alias_method :jump, :call
+class Object
+  def returning(value)
+    yield(value)
+    value
+  end
+end
+
+class Symbol
+  def to_proc
+    Proc.new { |*args| args.shift.__send__(self, *args) }
+  end
 end
 
 class Method
   # Returns array of values yielded by method.
   # Example:
-  #   2.method(:times).yields # => [0, 1]
+  #   3.method(:times).yields # => [0, 1, 2]
   def yields(*args)
     array = []
     call(*args, &array.method(:<<))
@@ -20,32 +30,38 @@ module Test
     module Assertions
       # Yields to block with proc argument to get each value yielded by given
       # method and raises assertion error unless method yields exact number of
-      # times required by block.
-      # Example:
+      # values required by block.
+      # Examples:
       #   assert_method_yields 2.method(:times) do |p|
       #     assert_equal 0, p.call
       #     assert_equal 1, p.call
       #   end
-      def assert_method_yields(m, *args)
-        c1 = nil
-        callcc do |c2|
-          p2 = proc do
-            c2, arg = callcc do |c1|
-              c2.jump(c1)
+      #   assert_method_yields [1, 2, 3].method(:inject), 0 do |p|
+      #     assert_equal [0, 1], p.call(&:+)
+      #     assert_equal [1, 2], p.call(&:+)
+      #     assert_equal [3, 3], p.call(&:+)
+      #   end
+      def assert_method_yields(method, *args)
+        block_c, block_f = callcc do |method_c|
+          p = lambda do |&block_f|
+            method_c, values = callcc do |block_c|
+              method_c.call(block_c, block_f)
             end
-            arg
+            return *values
           end
-          yield p2 if block_given?
-          c2.jump
+          yield p if block_given?
+          method_c.call(nil, nil)
         end
-        p1 = lambda do |arg|
-          c1 = callcc do |c2|
-            raise AssertionFailedError, "unexpected yield: #{arg.inspect}" unless c1
-            c1.jump(c2, arg)
+        result = method.call(*args) do |*values|
+          raise AssertionFailedError, "unexpected yield: #{values.map(&:inspect) * ", "}" unless block_c
+          returning block_f && block_f.call(*values) do
+            block_c, block_f = callcc do |method_c|
+              block_c.call(method_c, values)
+            end
           end
         end
-        m.call(*args, &p1)
-        raise AssertionFailedError, "yield missing" if c1
+        raise AssertionFailedError, "yield missing" if block_c
+        result
       end
     end
   end
--- test/assert_yields_test.rb
+++ test/assert_yields_test.rb
@@ -1,24 +1,31 @@
-# Copyright (c) 2007, 2008 Pythonic Pty. Ltd. http://www.pythonic.com.au/
+# Copyright (c) 2007, 2008 Pythonic Pty Ltd
+# http://www.pythonic.com.au/
 
 require "test/unit"
 require File.dirname(__FILE__) + "/../init.rb"
 
 class Integer
-  # Iterates block int times, passing in values from zero to int - 1.
+  # Yields to block with values from zero to self - 1, and returns self.
   def times
     k = 0
     while k < self
       yield k
       k += 1
     end
+    return self
   end
 end
 
 class TestAssertYields < Test::Unit::TestCase
   # These tests assume Integer#times is implemented correctly.
 
+  def yield_then_raise
+    yield
+    raise
+  end
+
   def test_yields
-    assert_equal [0, 1], 2.method(:times).yields
+    assert_equal [0, 1, 2], 3.method(:times).yields
   end
 
   def test_zero_yields
@@ -52,5 +59,69 @@ class TestAssertYields < Test::Unit::TestCase
         p.call
       end
     end
+  end
+
+  def test_assertion_failed
+    assert_raises Test::Unit::AssertionFailedError do
+      assert_method_yields 1.method(:times) do |p|
+        assert_equal 1, p.call
+      end
+    end
+  end
+
+  def test_method_first_exception
+    assert_raises RuntimeError do
+      assert_method_yields method(:raise) do |p|
+      end
+    end
+  end
+
+  def test_method_second_exception
+    assert_raises RuntimeError do
+      assert_method_yields method(:yield_then_raise) do |p|
+        p.call
+      end
+    end
+  end
+
+  def test_block_first_exception
+    assert_raises RuntimeError do
+      assert_method_yields 0.method(:times) do |p|
+        raise
+      end
+    end
+  end
+
+  def test_block_second_exception
+    assert_raises RuntimeError do
+      assert_method_yields 1.method(:times) do |p|
+        p.call
+        raise
+      end
+    end
+  end
+
+  def test_error_precedence
+    assert_raises Test::Unit::AssertionFailedError do
+      assert_method_yields method(:raise) do |p|
+        assert_equal 0, 1
+      end
+    end
+  end
+
+  def test_synchronized
+    assert_raises Test::Unit::AssertionFailedError do
+      assert_method_yields method(:yield_then_raise) do |p|
+      end
+    end
+  end
+
+  def test_inject
+    result = assert_method_yields [1, 2, 3].method(:inject), 0 do |p|
+      assert_equal [0, 1], p.call(&:+)
+      assert_equal [1, 2], p.call(&:+)
+      assert_equal [3, 3], p.call(&:+)
+    end
+    assert_equal 6, result
   end
 end
